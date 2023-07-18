3c01de11229c57d4ca339e48261db31872278734
ghccaixeta <gustavohcaixeta@gmail.com>
2023-07-17 21:15:37 -0300

    Component Library - Imagem de Background

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
-import { StatusBar, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { Box, NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider } from 'native-base';
 import { Loading } from '@components/Loading';
 
 import { THEME } from './src/theme';
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
 
@@ -17,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <View /> : <Loading />}
+      { fontsLoaded ? <SignIn /> : <Loading />}
       
     </NativeBaseProvider>
     
