5b465f54573abe58c908a7da3d1dacf741aac1a7
ghccaixeta <gustavohcaixeta@gmail.com>
2023-07-19 19:18:03 -0300

    Component Library - Criando a SignUp

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { NativeBaseProvider } from 'native-base';
 import { Loading } from '@components/Loading';
 
 import { THEME } from './src/theme';
-import { SignIn } from '@screens/SignIn';
+import { SignUp } from '@screens/SignUp';
 
 export default function App() {
 
@@ -18,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <SignIn /> : <Loading />}
+      { fontsLoaded ? <SignUp /> : <Loading />}
       
     </NativeBaseProvider>
     
