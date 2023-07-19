e15711f1b83f76446d80b4947c962e3e029fe3ce
ghccaixeta <gustavohcaixeta@gmail.com>
2023-07-18 21:01:26 -0300

    Component Library - Componente Heading

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { Center, Image, Text, VStack } from "native-base";
+import { Center, Heading, Image, Text, VStack } from "native-base";
 
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png';
@@ -20,6 +20,13 @@ export function SignIn() {
                     Treine sua mente e o seu corpo.
                 </Text>
 
+            </Center>
+
+            <Center>
+
+                <Heading color={"gray.100"} fontSize={"xl"} mb={6} fontFamily={"heading"}>
+                    Acesse sua conta
+                </Heading>
             </Center>
 
         </VStack>
