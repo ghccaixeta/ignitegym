6f516d9ef49f44518d7d2fa3daf549e86712b435
ghccaixeta <gustavohcaixeta@gmail.com>
2023-07-20 19:00:57 -0300

    Rotas Públicas - Criando as Rotas

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,16 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SignIn } from "@screens/SignIn";
+import { SignUp } from "@screens/SignUp";
+
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AuthRoutes() {
+  return (
+    <Navigator screenOptions={{ headerShown:false }} initialRouteName="sigin">
+      <Screen name="signin" component={SignIn} />
+      <Screen name="signup" component={SignUp} />
+      
+    </Navigator>
+  );
+}
