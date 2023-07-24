de8109062723ac37a4b2f760d34526c87366e515
ghccaixeta <gustavohcaixeta@gmail.com>
2023-07-24 18:45:18 -0300

    Rotas Privadas - Removendo o Header

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -17,7 +17,7 @@ const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
     return (
-        <Navigator>
+        <Navigator screenOptions={{ headerShown:false }}>
             <Screen
                 name="home"
                 component={Home}
