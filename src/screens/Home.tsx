0b555da2c1819680dc480d3a8a152aa61c930d8f
ghccaixeta <gustavohcaixeta@gmail.com>
2023-08-03 08:22:56 -0300

    Finalizando as Interfaces - Criando componente Group

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
 import { Text, Center, VStack } from 'native-base'
 
@@ -5,6 +6,7 @@ export function Home() {
     return(
         <VStack flex={1}>
             <HomeHeader />
+            <Group name='Name' />
         </VStack>
     )
     
