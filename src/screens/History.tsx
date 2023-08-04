e7b3f9bb5ab030f63946776d232bfecfd340e07d
ghccaixeta <gustavohcaixeta@gmail.com>
2023-08-04 20:40:03 -0300

    Finalizando as Interfaces - Componente ScreenHeader

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,12 +1,11 @@
-import { Text, Center } from 'native-base'
+import { ScreenHeader } from '@components/ScreenHeader'
+import { VStack } from 'native-base'
 
 export function History() {
     return(
-        <Center flex={1}>
-            <Text color={"white"}>
-                History
-            </Text>
-        </Center>
+        <VStack flex={1}>
+            <ScreenHeader title="Histórico de Exercícios"></ScreenHeader>
+        </VStack>
     )
     
 }
