a4891bb3a5c063346adf2c7d0b570758b173ad3c
ghccaixeta <gustavohcaixeta@gmail.com>
2023-08-04 08:29:15 -0300

    Finalizando as Interfaces - Estilizando evento de Pressed

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,17 @@
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-import { Text, Center, VStack } from 'native-base'
+import { Text, Center, VStack, HStack } from 'native-base'
 
 export function Home() {
     return(
         <VStack flex={1}>
             <HomeHeader />
-            <Group name='Name' />
+            <HStack>
+                <Group name='Costa' />
+                <Group name='Ombro' />
+                <Group name='Pernas' />
+
+            </HStack>
         </VStack>
     )
     
