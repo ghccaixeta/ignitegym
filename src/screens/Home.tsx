176d83275e1224e86e22afee0e72ca017f48cb86
ghccaixeta <gustavohcaixeta@gmail.com>
2023-08-04 08:50:17 -0300

    Finalizando as Interfaces - Header da lista de exercícios

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,6 @@
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-import { Text, Center, VStack, HStack, FlatList } from 'native-base'
+import { Text, Center, VStack, HStack, FlatList, Heading } from 'native-base'
 import { useState } from 'react'
 
 export function Home() {
@@ -8,23 +8,35 @@ export function Home() {
     const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombros'])
     const [groupSelected, setGroupSelected] = useState('costa');
 
-    return(
+    return (
         <VStack flex={1}>
             <HomeHeader />
-            <FlatList 
+            <FlatList
                 data={groups}
                 keyExtractor={item => item}
-                renderItem={({item}) =>(
-                    <Group  name={item} isActive={groupSelected === item} onPress={()=>setGroupSelected(item)} />
+                renderItem={({ item }) => (
+                    <Group name={item} isActive={groupSelected === item} onPress={() => setGroupSelected(item)} />
                 )}
                 horizontal
                 showsHorizontalScrollIndicator={false}
-                _contentContainerStyle={{px: 8}}
+                _contentContainerStyle={{ px: 8 }}
                 my={10}
                 maxH={10}
             />
-           
+
+            <VStack flex={1} px={8}>
+
+                <HStack justifyContent={"space-between"} mb={5}>
+                    <Heading color={"gray.200"} fontSize={"md"}>
+                        Exercícios
+                    </Heading>
+                    <Text color={"gray.200"} fontSize={"sm"}>
+                        4
+                    </Text>
+                </HStack>
+            </VStack>
+
         </VStack>
     )
-    
+
 }
