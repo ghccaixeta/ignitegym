24b3f78763ac430553e80ce58c0c1ae45dd856bc
ghccaixeta <gustavohcaixeta@gmail.com>
2023-08-05 20:58:10 -0300

    Finalizando as Interfaces - Utilizando SectionList

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -2,7 +2,7 @@ import { HStack, Heading, VStack, Text } from "native-base";
 
 export function HistoryCard() {
     return(
-        <HStack justifyContent={"space-between"} alignItems={"center"} bg={"gray.600"} rounded={"md"} px={5} py={4}>
+        <HStack justifyContent={"space-between"} alignItems={"center"} bg={"gray.600"} rounded={"md"} px={5} py={4} mb={3}>
             <VStack mr={5}>
                 <Heading color={"white"} fontSize={"md"} textTransform={"capitalize"}>Costas</Heading>
                 <Text color={"gray.100"} fontSize={"lg"} numberOfLines={1}>Puxada Frontral</Text>
--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,13 +1,36 @@
 import { HistoryCard } from '@components/HistoryCard'
 import { ScreenHeader } from '@components/ScreenHeader'
-import { VStack } from 'native-base'
+import { Heading, VStack, SectionList } from 'native-base'
+import { useState } from 'react'
+
 
 export function History() {
-    return(
+    const [exercises, setExercises] = useState([
+        {
+            title: '26.08.22',
+            data: ['Puxada Frontal', 'Remada Unilateral'],
+        },
+        {
+            title: '27.08.22',
+            data: ['Puxada Frontal'],
+        }
+    ])
+    return (
         <VStack flex={1}>
             <ScreenHeader title="Histórico de Exercícios" />
-            <HistoryCard />
+            <SectionList
+                sections={exercises}
+                keyExtractor={item => item}
+                renderItem={({item}) => (<HistoryCard />)}
+                renderSectionHeader={({section})=>(
+                    <Heading color={"gray.100"} fontSize={"md"} mt={10} mb={3}>
+                        {section.title}
+                    </Heading>
+                )}
+                px={6}
+            />
+            
         </VStack>
     )
-    
+
 }
