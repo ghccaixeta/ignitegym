138522cb488bd73efdbd4db57a9c633cf4231541
ghccaixeta <gustavohcaixeta@gmail.com>
2023-08-08 08:16:58 -0300

    Finalizando as Interfaces - Recap da History

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -34,6 +34,7 @@ export function History() {
                         Nenhum exercício registado.
                     </Text>
                 )}
+                showsVerticalScrollIndicator={false}
             />
             
         </VStack>
