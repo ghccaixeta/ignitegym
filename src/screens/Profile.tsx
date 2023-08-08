5884aa5eed8fea20238ff8592fbff05dbdad5a27
ghccaixeta <gustavohcaixeta@gmail.com>
2023-08-08 08:29:40 -0300

    Finalizando as Interfaces - Foto do usuário

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,11 +1,23 @@
 import { ScreenHeader } from '@components/ScreenHeader'
-import { VStack } from 'native-base'
+import { UserPhoto } from '@components/UserPhoto'
+import { Center, ScrollView, VStack } from 'native-base'
 
 export function Profile() {
-    return(
+    return (
         <VStack flex={1}>
             <ScreenHeader title='Perfil' />
+            <ScrollView>
+                <Center mt={6} px={10}>
+                    <UserPhoto
+                        source={{ uri: 'https://github.com/ghccaixeta.png' }}
+                        size={33}
+                        alt="Imagem do usuário"
+                        
+                    />
+
+                </Center>
+            </ScrollView>
         </VStack>
     )
-    
+
 }
