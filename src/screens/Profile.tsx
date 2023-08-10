96aefe5b943869157cbdfddf4d711264c57217ad
ghccaixeta <gustavohcaixeta@gmail.com>
2023-08-10 08:18:28 -0300

    Exibindo a foto selecionada

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -11,7 +11,7 @@ const PHOTO_SIZE = 33;
 
 export function Profile() {
     const [photoIsLoading, setPhotoIsLoading] = useState(false);
-
+    const [userPhoto, setUserPhoto] = useState('https://github.com/ghccaixeta.png');
     async function handleUserPhotoSelect() {
         const photoSelected = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.Images,
@@ -21,7 +21,10 @@ export function Profile() {
 
         });
 
-        console.log(photoSelected)
+        if(photoSelected.canceled) return
+
+        setUserPhoto(photoSelected.assets[0].uri)
+        
     }
 
     return (
@@ -40,7 +43,7 @@ export function Profile() {
                             />
                             :
                             <UserPhoto
-                                source={{ uri: 'https://github.com/ghccaixeta.png' }}
+                                source={{ uri: userPhoto }}
                                 size={PHOTO_SIZE}
                                 alt="Imagem do usuário"
 
