e3168a3b0b06762eddcb0af7bb9c2afc3839e3d2
ghccaixeta <gustavohcaixeta@gmail.com>
2023-08-17 07:33:07 -0300

    Async Await

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -37,19 +37,19 @@ export function SignUp() {
         navigation.navigate('signIn');
     }
 
-    function handleSignUp({ name, email, password }: FormDataProps) {
-        fetch('http://192.168.3.61:3333/users', {
+    async function handleSignUp({ name, email, password }: FormDataProps) {
+        const response = await fetch('http://192.168.3.61:3333/users', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({ name, email, password })
-        }).then(
-            response => response.json()
-        ).then(
-            data => console.log(data)
-        )
+        })
+
+        const data = await response.json()
+
+        console.log(data)
     }
 
     return (
