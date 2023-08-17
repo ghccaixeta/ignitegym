10c646afa64d974bb8c1590d58a01b3372cd8878
ghccaixeta <gustavohcaixeta@gmail.com>
2023-08-17 07:26:56 -0300

    Cadastrando usuário

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -37,8 +37,15 @@ export function SignUp() {
         navigation.navigate('signIn');
     }
 
-    function handleSignUp(data: FormDataProps) {
-        console.log(data)
+    function handleSignUp({name, email, password}: FormDataProps) {
+        fetch('http://192.168.3.61:3333/users',{
+            method: 'POST',
+            headers:{
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({name, email, password})
+        })
     }
 
     return (
