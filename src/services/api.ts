21d2249dbe61d4b683aeb7da3c373620622ff6bc
ghccaixeta <gustavohcaixeta@gmail.com>
2023-08-17 08:09:27 -0300

    Entendendo os Interceptors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,15 @@
 import axios from "axios";
 
-export const api = axios.create({
+const api = axios.create({
     baseURL: 'http://192.168.3.61:3333'
 })
+
+api.interceptors.response.use((response) => {
+    console.log("RESPONSE = ", response)
+    return response
+}, (error) => {
+    console.log("RESPONSE ERROR = ", error)
+    return Promise.reject(error)
+})
+
+export { api }
