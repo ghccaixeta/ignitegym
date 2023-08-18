36e025fd264d8fda9b530ed5000e7809ef300284
ghccaixeta <gustavohcaixeta@gmail.com>
2023-08-18 07:51:01 -0300

    Unificando o create e provider context

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,13 @@
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { AuthRoutes } from './auth.routes';
 import { Box, useTheme } from 'native-base';
-import { AppRoutes } from './app.routes';
-import { useContext } from 'react';
-import { AuthContext } from '@contexts/AuthContext';
+import { useAuth } from '@hooks/useAuth';
 
 export function Routes() {
 
     const { colors } = useTheme();
 
-    const contextData = useContext(AuthContext)
-
-    console.log(AuthContext);
+    const {user} = useAuth();
 
     const theme = DefaultTheme;
     theme.colors.background = colors.gray[700];
