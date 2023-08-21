512dbee122440171e499fc0c5766a90741a33a30
ghccaixeta <gustavohcaixeta@gmail.com>
2023-08-21 08:41:59 -0300

    Redirecionar usuário logado

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,14 +2,13 @@ import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { AuthRoutes } from './auth.routes';
 import { Box, useTheme } from 'native-base';
 import { useAuth } from '@hooks/useAuth';
+import { AppRoutes } from './app.routes';
 
 export function Routes() {
 
     const { colors } = useTheme();
 
     const {user} = useAuth();
-
-    console.log(user)
 
     const theme = DefaultTheme;
     theme.colors.background = colors.gray[700];
@@ -18,7 +17,7 @@ export function Routes() {
         <Box flex={1} bg={"gray.700"}>
 
             <NavigationContainer>
-                <AuthRoutes />
+                { user.id ? <AppRoutes /> : <AuthRoutes />}
             </NavigationContainer>
 
         </Box>
