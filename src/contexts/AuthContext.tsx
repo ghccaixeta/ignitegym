695c8263fc182a81dcc3d943127ed373b0c2228f
ghccaixeta <gustavohcaixeta@gmail.com>
2023-08-28 08:43:52 -0300

    Anexando token nas requisições

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { UserDTO } from "@dtos/UserDTO";
 import { api } from "@services/api";
+import { storageAuthTokenSave } from "@storage/storageAuthToken";
 import { storageUserDelete, storageUserGet, storageUserSave } from "@storage/storageUser";
 import { ReactNode, createContext, useEffect, useState } from "react";
 
@@ -21,14 +22,36 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     const [user, setUser] = useState<UserDTO>({} as UserDTO)
     const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true);
 
+
+    async function storageUserAndToken(userData: UserDTO, token: string) {
+        
+        try {
+            
+            setIsLoadingUserStorageData(true)
+
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+            setUser(userData);
+            await storageUserSave(userData);
+            await storageAuthTokenSave(token);
+            
+        } catch (error) {
+            throw error;
+        } finally {
+            setIsLoadingUserStorageData(false)
+        }
+
+        
+    }
+
     async function signIn(email: string, password: string) {
         try {
             
             const { data } = await api.post('/sessions', {email, password});
 
             if(data.user && data.token){
-                setUser(data.user)
-                storageUserSave(data.user);
+                storageUserAndToken(data.user, data.token);
+
             }
 
         } catch (error) {
