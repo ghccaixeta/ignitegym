27eef8966fdaa761496294f555ada877ca002533
ghccaixeta <gustavohcaixeta@gmail.com>
2023-09-05 08:59:34 -0300

    Carregando dados do usuário no profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -7,37 +7,54 @@ import { useState } from 'react';
 import { Alert, TouchableOpacity } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
+import { Controller, useForm } from 'react-hook-form';
+import { useAuth } from '@hooks/useAuth';
 
 
 const PHOTO_SIZE = 33;
+
+type FormDataProps = {
+    name: string;
+    email: string;
+    old_password: string;
+    confirm_password: string;
+
+}
 
 export function Profile() {
     const [photoIsLoading, setPhotoIsLoading] = useState(false);
     const [userPhoto, setUserPhoto] = useState('https://github.com/ghccaixeta.png');
 
     const toast = useToast();
+    const { user } = useAuth();
+    const { control } = useForm<FormDataProps>({
+        defaultValues:{
+            name: user.name,
+            email: user.email
+        }
+    });
 
     async function handleUserPhotoSelect() {
         setPhotoIsLoading(true)
         try {
-            
+
             const photoSelected = await ImagePicker.launchImageLibraryAsync({
                 mediaTypes: ImagePicker.MediaTypeOptions.Images,
                 quality: 1,
-                aspect: [4,4],
+                aspect: [4, 4],
                 allowsEditing: true,
-    
+
             });
-    
-            if(photoSelected.canceled) return
-    
-            if(photoSelected.assets[0].uri){
+
+            if (photoSelected.canceled) return
+
+            if (photoSelected.assets[0].uri) {
 
                 const uri = photoSelected.assets[0].uri
 
                 const photoInfo = await FileSystem.getInfoAsync(uri) as FileSystem.FileInfo
 
-                if(photoInfo.size && (photoInfo.size / 1024 / 1024 > 2)  ){
+                if (photoInfo.size && (photoInfo.size / 1024 / 1024 > 2)) {
 
                     return toast.show({
                         title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
@@ -45,7 +62,7 @@ export function Profile() {
                         bgColor: "red.500"
                     })
 
-                    
+
                 }
 
                 setUserPhoto(uri)
@@ -53,18 +70,18 @@ export function Profile() {
 
         } catch (error) {
             console.log(error)
-        }finally{
+        } finally {
             setPhotoIsLoading(false)
 
         }
-        
+
     }
 
 
     return (
         <VStack flex={1}>
             <ScreenHeader title='Perfil' />
-            <ScrollView contentContainerStyle={{paddingBottom: 36}}>
+            <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
                 <Center mt={6} px={10}>
                     {
                         photoIsLoading ?
@@ -90,32 +107,51 @@ export function Profile() {
                         </Text>
                     </TouchableOpacity>
 
-                    <Input 
-                        bg={"gray.600"}
-                        placeholder='Nome'
+                    <Controller
+                        control={control}
+                        name='name'
+                        render={({ field: { value, onChange } }) => (
+
+                            <Input
+                                bg={"gray.600"}
+                                placeholder='Nome'
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                        )}
                     />
-                    <Input 
-                        bg={"gray.600"}
-                        placeholder='E-mail'
-                        isDisabled
+
+                    <Controller
+                        control={control}
+                        name='email'
+                        render={({ field: { value, onChange } }) => (
+
+                            <Input
+                                bg={"gray.600"}
+                                placeholder='E-mail'
+                                isDisabled
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                        )}
                     />
 
                     <Heading color={"gray.200"} fontSize={"md"} mb={2} alignSelf={"flex-start"} mt={12} fontFamily={"heading"}>
                         Alterar Senha
                     </Heading>
-                    <Input 
+                    <Input
                         bg={"gray.600"}
                         placeholder='Senha antiga'
                         secureTextEntry
                         ml={0}
                     />
-                    <Input 
+                    <Input
                         bg={"gray.600"}
                         placeholder='Nova aenha'
                         secureTextEntry
                         ml={0}
                     />
-                    <Input 
+                    <Input
                         bg={"gray.600"}
                         placeholder='Confirme a nova senha'
                         secureTextEntry
@@ -123,8 +159,8 @@ export function Profile() {
                     />
                     <Button title='Atualizar' />
                 </Center>
-                
-                
+
+
             </ScrollView>
         </VStack>
     )
