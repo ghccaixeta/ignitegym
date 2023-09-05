70ae541264674723dd60bc922cfcc55595cc06ef
ghccaixeta <gustavohcaixeta@gmail.com>
2023-09-05 08:45:01 -0300

    Exibindo dados do histórico

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -1,13 +1,18 @@
+import { HistoryDTO } from "@dtos/HistoryDTO";
 import { HStack, Heading, VStack, Text } from "native-base";
 
-export function HistoryCard() {
+type Props = {
+    data: HistoryDTO;
+}
+
+export function HistoryCard({ data }: Props) {
     return(
         <HStack justifyContent={"space-between"} alignItems={"center"} bg={"gray.600"} rounded={"md"} px={5} py={4} mb={3}>
             <VStack mr={5} flex={1}>
-                <Heading color={"white"} fontSize={"md"} textTransform={"capitalize"} fontFamily={"heading"}>Costas</Heading>
-                <Text color={"gray.100"} fontSize={"lg"} numberOfLines={1}>Puxada Frontral</Text>
+                <Heading color={"white"} fontSize={"md"} textTransform={"capitalize"} fontFamily={"heading"}>{data.group}</Heading>
+                <Text color={"gray.100"} fontSize={"lg"} numberOfLines={1}>{data.name}</Text>
             </VStack>
-            <Text color={"gray.300"} fontSize={"md"}>08:56</Text>
+            <Text color={"gray.300"} fontSize={"md"}>{data.hour}</Text>
         </HStack>
     )
 }
