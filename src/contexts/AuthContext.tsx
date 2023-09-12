f845fbd6be276c335d473fa805c2f44f498a3354
ghccaixeta <gustavohcaixeta@gmail.com>
2023-09-12 08:51:09 -0300

    Atualizando dados do usuário no storage e estado

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -8,6 +8,7 @@ export type AuthContextDataProps = {
     user: UserDTO;
     signIn: (email: string, password: string) => Promise<void>;
     signOut: () => Promise<void>;
+    updateUserProfile: (userUpdated: UserDTO) => Promise<void>;
     isLoadingUserStorageData: boolean
 }
 
@@ -88,6 +89,15 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
         }
     }
 
+    async function updateUserProfile(userUpdated: UserDTO){
+        try {
+            setUser(userUpdated);
+            await storageUserSave(userUpdated);
+        } catch (error) {
+            throw error;
+        }
+    }
+
     async function loadUserData() {
         try {
 
@@ -123,6 +133,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
             signIn,
             isLoadingUserStorageData,
             signOut,
+            updateUserProfile
         }}>
             {children}
         </AuthContext.Provider>
